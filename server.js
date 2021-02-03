a2b47fa983cc0ca8c9779766d3c7b6fcfa9f7938
Matthew Lee <laughew@gmail.com>
2021-02-02 22:45:37 -0800

    fixed bike-disconnect event

--- server.js
+++ server.js
@@ -12,20 +12,25 @@ const server = express()
     .listen(PORT, () => console.log(`listening on ${PORT}`));
 
 const io = socketIO(server);
+var BIKE_CLIENT_ID = "";
 
 io.on('connection', (socket) => {
   console.log(`client connected: ${socket.id}`);
     
   socket.on('bike-connect', () => {
     io.emit('bike-connect');
+    BIKE_CLIENT_ID = socket.id;
   });
-  socket.on('bike-disconnect', () => {
-    io.emit('bike-disconnect');
-  });
+
   socket.on('update-data', (speed, temp, rpm) => {
     io.emit('update-data', speed, temp, rpm);
   });
-  socket.on('disconnect', () => console.log('client disconnected'));
+  socket.on('disconnect', () => {
+    if (socket.id = BIKE_CLIENT_ID) {
+      io.emit('bike-disconnect');
+    }
+    console.log('client disconnected')
+  });
 });
 
 
