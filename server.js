aca0998d182473bc080d369a9aecdecdf7dec59c
Matthew Lee <laughew@gmail.com>
2021-03-30 17:43:38 -0700

    added comments

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 
 'use strict';
 
+// require statements to load libraries
 const express = require('express');
 const socketIO = require('socket.io');
 
+// port determined by process environment, default to 3000
 const PORT = process.env.PORT || 3000;
+
+// webpage filename
 const INDEX = '/index.html';
 
+// initializes express server to serve webpage to clients
 const server = express()
     .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
     .listen(PORT, () => console.log(`listening on ${PORT}`));
 
+// initializes websocket
 const io = socketIO(server);
 var BIKE_CLIENT_ID = "";
 
+// handlers for socket events
 io.on('connection', (socket) => {
   console.log(`client connected: ${socket.id}`);
     
